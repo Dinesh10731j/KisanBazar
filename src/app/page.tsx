7bcc5c91eea4360506ad7586ae1487983459cd61
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-03-31 17:45:44 +0545

    Add new pages for Admin, Customer, Farmer, Layout, Dashboard, About, Blog, Contact, Faq, Home, and Products; update button gradient in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <>
-  <Button  className="bg-[linear-gradient(45deg,_var(--farm-green),_var(--earth-brown),_var(--sun-yellow)))] text-white mt-2 ml-3 p-4 rounded-lg" >Welcome To KisanBazar</Button>
+  <Button  className="bg-[linear-gradient(_var(--farm-green),_var(--earth-brown),_var(--sun-yellow)))] text-white mt-2 ml-3 p-4 rounded-lg" >Welcome To KisanBazar</Button>
 <Footer/>
    </>
   );
